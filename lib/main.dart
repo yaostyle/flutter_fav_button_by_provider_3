1f2f172d52b66f93fb5830062ab06b83143249c9
Chris Hsu <yaostyle@gmail.com>
2019-07-16 09:49:14 -0700

    Implmenet animation

--- lib/main.dart
+++ lib/main.dart
@@ -53,12 +53,53 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class FavButton extends StatelessWidget {
+class FavButton extends StatefulWidget {
   const FavButton({Key key}) : super(key: key);
+
+  @override
+  _FavButtonState createState() => _FavButtonState();
+}
+
+class _FavButtonState extends State<FavButton>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+
+  FavIcon favIcon;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1500),
+      lowerBound: 0.2,
+      upperBound: 1.5,
+    );
+
+    _animationController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  void _onTapDown(TapDownDetails details, bool status) {
+    setState(() {
+      favIcon = FavIcon(status, 1.3);
+    });
+    _animationController.fling();
+  }
+
+  void _onTapUp(TapUpDetails details, bool status) {
+    setState(() {
+      favIcon = FavIcon(status, 1.0);
+    });
+    _animationController.reverse();
+  }
 
   @override
   Widget build(BuildContext context) {
     final fav = Provider.of<Fav>(context);
+    favIcon = FavIcon(fav.status, 1.0);
     return Container(
       color: Colors.pink,
       child: Center(
@@ -68,13 +109,9 @@ class FavButton extends StatelessWidget {
           children: <Widget>[
             GestureDetector(
               onTap: Provider.of<Fav>(context).update,
-              child: Icon(
-                fav.status != null && fav.status == true
-                    ? Icons.favorite
-                    : Icons.favorite_border,
-                size: 300.0,
-                color: Colors.white,
-              ),
+              onTapDown: (td) => _onTapDown(td, fav.status),
+              onTapUp: (tu) => _onTapUp(tu, fav.status),
+              child: favIcon,
             ),
             CounterLabel(),
           ],
@@ -91,6 +128,24 @@ class CounterLabel extends StatelessWidget {
     return Text(
       "You've clicked ${fav.count} times...",
       style: TextStyle(fontSize: 24, color: Colors.white),
+    );
+  }
+}
+
+class FavIcon extends StatelessWidget {
+  final bool fav;
+  final double scaleNum;
+  const FavIcon(this.fav, this.scaleNum);
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.scale(
+      scale: scaleNum,
+      child: Icon(
+        fav != null && fav == true ? Icons.favorite : Icons.favorite_border,
+        size: 300.0,
+        color: Colors.white,
+      ),
     );
   }
 }
